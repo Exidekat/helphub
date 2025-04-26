7e2b9fd517f8f695d8024b19d6aa208d5d7ee247
Haydon Behl <exidekat@gmail.com>
2025-04-26 14:28:33 -0700

    fix home

--- frontend/App.tsx
+++ frontend/App.tsx
@@ -1,16 +1,18 @@
 // frontend/App.tsx
-import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import HomePage from './pages/HomePage'
-import ScanPage from './pages/ScanPage'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ScanPage from './pages/ScanPage';
 
 const App: React.FC = () => (
-  <BrowserRouter basename={import.meta.env.BASE_URL || '/'}>
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/scan/:code_id" element={<ScanPage />} />
-    </Routes>
+  <BrowserRouter>
+    <div className="min-h-screen bg-primary text-text">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/scan/:code_id" element={<ScanPage />} />
+      </Routes>
+    </div>
   </BrowserRouter>
-)
+);
 
-export default App
+export default App;
